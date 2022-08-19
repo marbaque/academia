5720199999e2b277e72b6d17684ed8a3c2f089c4
mbadilla <mario.badilla@gmail.com>
2022-08-19 09:07:46 -0600

    delete plantillas LP sin usar

--- functions.php
+++ functions.php
@@ -10,7 +10,7 @@
 
 if (!defined('academia_version')) {
 	// Replace the version number of the theme on each release.
-	define('academia_version', '1.1.4');
+	define('academia_version', '1.1.5');
 }
 
 if (!function_exists('academia_setup')) :
