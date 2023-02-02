74d0277f8a464f8bd77907e52d1381a3c05effb8
mario <mario.badilla@gmail.com>
2023-02-01 21:06:38 -0600

    update theme to 4.0.2

--- learnpress/single-course/sidebar/user-time.php
+++ learnpress/single-course/sidebar/user-time.php
@@ -4,32 +4,37 @@
  *
  * @author  ThimPress
  * @package LearnPress/Templates
- * @version 4.0.1
+ * @version 4.0.2
  */
 
 defined( 'ABSPATH' ) || exit;
 
+if ( ! isset( $data ) || ! isset( $data['start_time'] ) || ! isset( $data['end_time'] )
+	|| ! isset( $data['status'] ) || ! isset( $data['expiration_time'] ) ) {
+	return;
+}
+
 /**
- * @var string      $status
  * @var LP_Datetime $start_time
  * @var LP_Datetime $end_time
  * @var LP_Datetime $expiration_time
  */
-$time   = current_time( 'mysql', true );
-$user   = LP_Global::user();
-$course = LP_Global::course();
+$start_time      = $data['start_time'];
+$end_time        = $data['end_time'];
+$status          = $data['status'];
+$expiration_time = $data['expiration_time'];
 ?>
 
 <div class="course-time">
 	<p class="course-time-row">
 		<strong><?php esc_html_e( 'You started on:', 'learnpress' ); ?></strong>
-		<time class="entry-date enrolled"><?php echo $start_time->format( 'i18n' ); ?></time>
+		<time class="entry-date enrolled"><?php echo esc_html( $start_time->format( 'i18n' ) ); ?></time>
 	</p>
 	<?php if ( in_array( $status, array( learn_press_user_item_in_progress_slug(), 'enrolled' ) ) ) : ?>
 		<?php if ( $expiration_time ) : ?>
 			<p class="course-time-row">
 				<strong><?php esc_html_e( 'Course will end:', 'learnpress' ); ?></strong>
-				<time class="entry-date expire"><?php echo $expiration_time->format( 'i18n' ); ?></time>
+				<time class="entry-date expire"><?php echo esc_html( $expiration_time->format( 'i18n' ) ); ?></time>
 			</p>
 		<?php else : ?>
 			<p class="course-time-row">
@@ -40,7 +45,7 @@ $course = LP_Global::course();
 	<?php elseif ( $status === 'finished' && $end_time ) : ?>
 		<p class="course-time-row">
 			<strong><?php esc_html_e( 'You finished on:', 'learnpress' ); ?></strong>
-			<time class="entry-date finished"><?php echo $end_time->format( 'i18n' ); ?></time>
+			<time class="entry-date finished"><?php echo esc_html( $end_time->format( 'i18n' ) ); ?></time>
 		</p>
 	<?php endif; ?>
 </div>
